3580a2517096b238751c8820d7ce69370b522a99
beatific-media-1104 <riostechnologies@gmail.com>
2020-01-03 02:42:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,16 +5,17 @@
 
 
 
-def introduction = name
+def introduction = (name)
   "Hi my name is #{name}."
 end
 
-def introduction_with_language = name, language
+def introduction_with_language = (name,language)
   "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional = name, language 
+def introduction_with_language_optional = (name,language = "Ruby") 
   "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
